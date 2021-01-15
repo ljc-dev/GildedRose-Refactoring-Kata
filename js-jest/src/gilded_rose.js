749b94917f74c23bf0d19a5c6516b0eddca6553c
Simon Weller <s.meierbeck@gmail.com>
2021-01-16 00:23:27 +0100

    Undo negation and reorder third code block

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -28,14 +28,12 @@ class Shop {
 			item.sellIn = item.sellIn - 1;
 
 			if (item.sellIn < 0) {
-				if (item.name !== 'Aged Brie') {
-					if (item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
-						this.decrementQualityDownTo0(item);
-					} else {
-						item.quality = item.quality - item.quality;
-					}
-				} else {
+				if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+					item.quality = 0;
+				} else if (item.name === 'Aged Brie') {
 					this.incrementQualityUpTo50(item);
+				} else {
+					this.decrementQualityDownTo0(item);
 				}
 			}
 		});
