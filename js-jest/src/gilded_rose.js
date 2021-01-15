7bbdb6d72da810147e3e086342c483fc6b42fe56
Simon Weller <s.meierbeck@gmail.com>
2021-01-15 23:50:15 +0100

    Return early for "Sulfuras, Hand of Ragnaros"

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -13,6 +13,8 @@ class Shop {
 
 	updateQuality() {
 		this.items.forEach(item => {
+			if (item.name === 'Sulfuras, Hand of Ragnaros') return;
+
 			if (item.name === 'Aged Brie') {
 				if (item.quality < 50) {
 					item.quality = item.quality + 1;
@@ -27,22 +29,19 @@ class Shop {
 						item.quality = item.quality + 1;
 					}
 				}
-			} else if (item.name === 'Sulfuras, Hand of Ragnaros') {
 			} else {
 				if (item.quality > 0) {
 					item.quality = item.quality - 1;
 				}
 			}
-			if (item.name !== 'Sulfuras, Hand of Ragnaros') {
-				item.sellIn = item.sellIn - 1;
-			}
+
+			item.sellIn = item.sellIn - 1;
+
 			if (item.sellIn < 0) {
 				if (item.name !== 'Aged Brie') {
 					if (item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
 						if (item.quality > 0) {
-							if (item.name !== 'Sulfuras, Hand of Ragnaros') {
-								item.quality = item.quality - 1;
-							}
+							item.quality = item.quality - 1;
 						}
 					} else {
 						item.quality = item.quality - item.quality;
