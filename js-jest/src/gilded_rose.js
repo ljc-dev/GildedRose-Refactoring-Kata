dd9836eb3ead737df35f2ffb38e3349864ea6ea4
Simon Weller <s.meierbeck@gmail.com>
2021-01-16 00:19:44 +0100

    Shift guard clauses to helper methods

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -16,23 +16,13 @@ class Shop {
 			if (item.name === 'Sulfuras, Hand of Ragnaros') return;
 
 			if (item.name === 'Aged Brie') {
-				if (item.quality < 50) {
-					item.quality = item.quality + 1;
-				}
+				this.incrementQualityUpTo50(item);
 			} else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
-				if (item.quality < 50) {
-					item.quality = item.quality + 1;
-					if (item.sellIn < 11 && item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
-					if (item.sellIn < 6 && item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
-				}
+				this.incrementQualityUpTo50(item);
+				if (item.sellIn < 11) this.incrementQualityUpTo50(item);
+				if (item.sellIn < 6) this.incrementQualityUpTo50(item);
 			} else {
-				if (item.quality > 0) {
-					item.quality = item.quality - 1;
-				}
+				this.decrementQualityDownTo0(item);
 			}
 
 			item.sellIn = item.sellIn - 1;
@@ -40,21 +30,29 @@ class Shop {
 			if (item.sellIn < 0) {
 				if (item.name !== 'Aged Brie') {
 					if (item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
-						if (item.quality > 0) {
-							item.quality = item.quality - 1;
-						}
+						this.decrementQualityDownTo0(item);
 					} else {
 						item.quality = item.quality - item.quality;
 					}
 				} else {
-					if (item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
+					this.incrementQualityUpTo50(item);
 				}
 			}
 		});
 
 		return this.items;
+	}
+
+	decrementQualityDownTo0(item) {
+		if (item.quality > 0) {
+			item.quality = item.quality - 1;
+		}
+	}
+
+	incrementQualityUpTo50(item) {
+		if (item.quality < 50) {
+			item.quality = item.quality + 1;
+		}
 	}
 }
 
